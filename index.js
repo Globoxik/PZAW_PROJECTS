f27318649d958e8041f6ac47d7f263f75a653f81
Szymon Tabisz <sztabisz@wp.pl>
2025-09-29 07:07:26 +0000

    Adding basic necessary files

---
+++ index.js
@@ -0,0 +1,15 @@
+import { createServer } from 'node:http';
+
+const server = createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello world!');
+
+});
+
+const port = 8000;
+const host = "localhost";
+
+
+server.listen(port, host, () => {
+    console.log(`Server listening on http://${host}:${port}`);
+})
