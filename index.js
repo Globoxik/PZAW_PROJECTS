137bac8bcd127ffc1288e3e2f6e6f8d787a933a6
Globoxik <sztabisz@wp.pl>
2025-10-10 19:34:39 +0200

    finished up the task (somehow)

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 import { createServer } from 'node:http';
+import fs from 'node:fs';
 
+
+const index_html = fs.readFileSync("./index.html");
+const favicon_ico = fs.readFileSync("./favicon.ico");
 const server = createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello world!');
-
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(index_html);
+    }
+    else if (req.url === '/favicon.ico') {
+        res.writeHead(200, { 'Content-Type': 'image/vnd.microsoft.icon' });
+        res.end(favicon_ico);
+    }
 });
 
 const port = 8000;
 const host = "localhost";
 
-
 server.listen(port, host, () => {
     console.log(`Server listening on http://${host}:${port}`);
-})
+});
