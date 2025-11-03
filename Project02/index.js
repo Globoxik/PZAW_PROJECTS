d5e38abaa4dd0250aa12b4402ea8c3b8201fb179
Szymon Tabisz <sztabisz@wp.pl>
2025-11-03 08:33:51 +0000

    made minor adjustments

--- Project02/index.js
+++ Project02/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 const port = 8000;
-const card_categories = ["j. angielski - food", "stolice europejskie"];
+const my_objects = {
+  "Karcianka": {name:"Yu-Gi-Oh!",description:"Moja ulubiona karcianka TCG w którą gram na porządku dziennym"},
+  "Komputer":{name:"Laptop HP OMEN 17",description:"Laptop którego używam w moim pokoju"}
+};
 
 const app = express();
 
@@ -8,12 +11,14 @@ app.set("view engine", "ejs");
 
 app.use(express.static('public'));
 
-app.get("/cards/categories/", (req, res) => {
-  res.render("categories", {
-    title: "Kategorie",
-    categories: card_categories,
+app.get("/", (req, res) => {
+  res.render("root", {
+    title: "Zbiór wszystkich obecnych obiektów",
+    objects: my_objects
   });
 });
+
+
 
 app.listen(port, () => {
     console.log(`Server is listening on http://localhost:${port}`);
