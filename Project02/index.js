de14073a4b7b94e2f7c15f15e35ced2cc178109a
Globoxik <sztabisz@wp.pl>
2025-11-10 21:02:40 +0100

    Finished the 2nd Project

--- Project02/index.js
+++ Project02/index.js
@@ -1,25 +1,77 @@
-import express from 'express';
+import express from "express";
 const port = 8000;
+
 const my_objects = {
-  "Karcianka": {name:"Yu-Gi-Oh!",description:"Moja ulubiona karcianka TCG w którą gram na porządku dziennym"},
-  "Komputer":{name:"Laptop HP OMEN 17",description:"Laptop którego używam w moim pokoju"}
+  "Yu-Gi-Oh!": {
+    name: "Yu-Gi-Oh!",
+    description: "Moja ulubiona karcianka TCG w którą gram na porządku dziennym",
+  },
+  "Laptop HP OMEN 17": {
+    name: "Laptop HP OMEN 17",
+    description: "Laptop którego używam w moim pokoju",
+  },
 };
 
+function hasObject(id) {
+  return my_objects.hasOwnProperty(id);
+}
+
+function getObject(id) {
+  const fixedId = id.replace(/_/g, " ");
+  if (hasObject(fixedId)) {
+    return { id: fixedId, ...my_objects[fixedId] };
+  }
+  return null;
+}
+
+function addObject(id, obj) {
+  my_objects[id] = obj;
+}
+
+
 const app = express();
-
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
 
 app.get("/", (req, res) => {
   res.render("root", {
     title: "Zbiór wszystkich obecnych obiektów",
-    objects: my_objects
+    objects: Object.entries(my_objects).map(([id, category]) => ({
+      id: id.replace(/\s+/g, "_"),
+      ...category,
+    })),
   });
 });
 
+app.get("/:id", (req, res) => {
+  const details = getObject(req.params.id);
+  if (details != null) {
+    res.render("details", {
+      title: details.name,
+      details,
+    });
+  } else {
+    res.sendStatus(404);
+  }
+});
 
+app.post("/new", (req, res) => {
+  const { name, description } = req.body;
+
+  if (!name || !description) {
+    return res.status(400).send("Wszystkie pola są wymagane");
+  }
+
+  if (hasObject(name)) {
+    return res.status(400).send("Obiekt o tej nazwie już istnieje");
+  }
+
+  addObject(name, { name, description });
+  res.redirect("/");
+});
 
 app.listen(port, () => {
-    console.log(`Server is listening on http://localhost:${port}`);
+  console.log(`Server is listening on http://localhost:${port}`);
 });
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # PZAW_PROJECTS
 public repo for projects during PZAW lessons
+# Jak korzystać z plików
+Project 01:
+
+Project 02:Program obsługuje 3 kombinacje (scieżka + metoda)
+1. GET /
+2. GET /nazwa obiektu(jesli nazwa ma spacje nalezy dodać znak "_")
+3. POST /new(który służy do dodania nowych obiektów)
+Po włączeniu programu użytkownik zaczyna na 1 obslugiwanej kombinacji.
+Może użyć linku bądź wpisac w adres URL nazwe obiektu aby skorzystać z 2
+Wypełnić formularz dodania nowego obiektu aby skorzystać z 3
